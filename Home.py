747a5f5006f0a2f9174c6e799ecdd8747fa14926
Siddharthan Dileep <siddharthandileep@gmail.com>
2023-10-02 15:28:38 +0530

    Setup

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Real Estate Analytics, Recommendation and Price Prediction App👋")
